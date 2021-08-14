3c318c5c6e1065ff516c90494b4117deafbdf4fc
cloudsavara <cloudsa.vara@gmail.com>
2021-08-14 22:42:26 +0200

    test
    test

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hellow,Welcome to my page!"
+    return "Hellow,Welcome to my pages!"
 
 if __name__ == "__main__":
     app.run(debug=True)
